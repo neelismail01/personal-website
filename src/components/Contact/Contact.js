4efb415064a74bfe500080adf0ee22daecbc0730
Neel Ismail <neelismail01@gmail.com>
2021-07-28 17:36:46 -0400

    minor styling changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,8 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className="contact-section-container">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#ff5500" fill-opacity="1" d="M0,128L80,144C160,160,320,192,480,176C640,160,800,96,960,80C1120,64,1280,96,1360,112L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
+                <path fill="#ff5500" fill-opacity="1" d="M0,128L120,122.7C240,117,480,107,720,117.3C960,128,1200,160,1320,176L1440,192L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
                 </path>
             </svg>
             <div className="contact-header">
